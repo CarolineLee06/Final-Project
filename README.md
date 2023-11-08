58ce32c44c5e16d093cdebe677dd6aeb2a6c50ad
CarolineLee06 <Caroline.1604.Lee@gmail.com>
2023-11-08 17:35:48 -0600

    Proposal

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Final-Project
+
+The project code takes an imput from prompting the veiwer to choose either a random 
+prompt generated from the prompt list or a specific prompt from the prompt list 
+the program will then take the prompt and use it to bring up a pixel art image attached to the prompt. The pygame window will open and the program will draw the pixel art for the viewer. 
---
+++ proposal.md/proposal.py
@@ -0,0 +1,47 @@
+# Pixels and Pictures
+
+## Repository
+https://github.com/CarolineLee06/Final-Project
+
+## Description
+# creates a program that randomly generates or creates a pixel art window using pygame based on a prompt.
+# This relates to the digital art and media field through giving people a quick and easy way to generate an art piece. 
+
+
+## Features
+- # Choice to create a random selection or choose from a prompt.
+- # Program opens and asks viewer to choose random or prompts.
+#  Provides short list of prompts
+- # Creates a pixel art window based on imput
+- # Given the imput the program will take the imput and output a pixel art
+- # The pixel art will draw itsself once its been generated
+- # String arguement will be implemented so the pixel art is drawn on the screen once generated.
+
+## Challenges
+- # Make sure the prompts in the separate file correspond with the correct 
+    # pixel art and that the program outputs that sepcific pixel art
+- # Research pygame functions and the moviepy art functions
+- # Research how to make the pixel art be drawn in the window once its been generated.
+
+## Outcomes
+## Ideal Outcome:
+- # the ideal outcome is that the user will imput the prompt or the random prompt and the program will output a 
+# pixel drawing based on the prompt given.
+
+## Minimal Viable Outcome:
+- # The program takes the prompt imput and outputs a pixel art window in gamepy
+
+## Milestones
+
+- Week 1
+  1. # layout coding window and begin creating classes and main
+  2. # Finish main coding block
+  3. # Complete Readme file
+
+- Week 2
+  1. # Create File of random prompts
+  2. # finish class coding
+
+- Week N (Final)
+  1. # finalize and final optimizing
+  2. # Create final video 
