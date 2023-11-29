cbe4c2bedf5c906ba42057d8ccf27ef3ae385385
CarolineLee06 <Caroline.1604.Lee@gmail.com>
2023-11-28 22:16:19 -0600

    fixed some formatting Issues

--- src/pixeldraw.py
+++ src/pixeldraw.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.locals import *
 
 
@@ -30,19 +31,22 @@ def drawScreen(screen, grid, basicX, basicY):
 
 screen.fill((255, 255, 255))  
 
+
+
 while running:
     events = pygame.event.get()
     for event in events:
         if (event.type == pygame.QUIT) or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = 0
+        color = pygame.Color(random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255))
         if event.type == pygame.MOUSEBUTTONDOWN or clicking:  
             clicking = 1
             x, y = pygame.mouse.get_pos()
             xInGrid = int(x / basicX)
             yInGrid = int(y / basicY)
             grid[yInGrid][xInGrid] = 1 
-            pygame.draw.rect(screen, (0, 0, 0), (xInGrid * basicX, yInGrid * basicY, basicX, basicY))
-            pygame.display.flip()  
+            pygame.draw.rect(screen, color, (xInGrid * basicX, yInGrid * basicY, basicX, basicY))
+            pygame.display.flip() 
         if event.type == pygame.MOUSEBUTTONUP:
             clicking = 0
         if event.type == pygame.VIDEORESIZE:  
@@ -50,7 +54,6 @@ while running:
             height = event.h
             basicX = width / numberOfColumns
             basicY = height / numberOfRows
-            #print(width, height)
             screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)  
             screen.fill((255, 255, 255)) 
             drawScreen(screen, grid, basicX, basicY) 
